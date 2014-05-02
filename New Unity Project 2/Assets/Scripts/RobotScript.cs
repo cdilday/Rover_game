049ec3b51a62501277fa9857055c09d3c8e586ec
khhle <khhle@ucsc.edu>
2014-05-01 23:39:24 -0700

    First version prototype
    i have no idea what i'm doing... hope this work

---
+++ New Unity Project 2/Assets/Scripts/RobotScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotScript : MonoBehaviour {
+
+	//type 1: up
+	//type 2: down
+	//type 3: left
+	//type 4: right
+	public int directionType;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveScript mov =  GetComponent<MoveScript>();
+		if(directionType == 1)
+			mov.direction = new Vector2(0, 1);
+		else if(directionType == 2)
+			mov.direction = new Vector2(0, -1);
+		else if(directionType == 3)
+			mov.direction = new Vector2(-1, 0);
+		else if(directionType == 4)
+			mov.direction = new Vector2(1, 0);
+	}
+}
---
+++ New Unity Project 2/Assets/Scripts/clickDrop.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+
+
+//[RequireComponent(typeof(SphereCollider))]
+
+
+
+public class clickDrop : MonoBehaviour
+	
+{
+	
+	void OnMouseDrag()
+		
+	{
+		
+		Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		
+		point.z = gameObject.transform.position.z;
+		
+		gameObject.transform.position = point;
+		
+		Screen.showCursor = false;
+		
+	}
+	
+	
+	
+	void OnMouseUp()
+		
+	{
+		
+		Screen.showCursor = true;
+		
+	}
+	
+}
